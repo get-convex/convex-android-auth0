87fc3d543ee40a12389db8fd08941dc29a0f3658
Christian Wyglendowski <christian@dowski.com>
2024-08-08 12:46:29 -0400

    Initial split of Auth0 support from the Convex for Android library

---
+++ convex-auth0/src/test/java/dev/convex/android/auth0/Auth0ProviderTest.kt
@@ -0,0 +1,14 @@
+package dev.convex.android.auth0
+
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import strikt.api.expectThat
+import strikt.assertions.isTrue
+
+class Auth0ProviderTest {
+    @Test
+    fun `example test`() = runTest {
+
+        expectThat(false).isTrue()
+    }
+}
